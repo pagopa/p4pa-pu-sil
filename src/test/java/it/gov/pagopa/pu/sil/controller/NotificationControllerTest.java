4b9759b147699f0931c4111271fe751c27ad881d
mscarsel <marco.scarsella@nttdata.com>
2025-03-12 09:10:54 +0100

    [P4ADEV-2273] createSendNotification API tests

---
+++ src/test/java/it/gov/pagopa/pu/sil/controller/NotificationControllerTest.java
@@ -0,0 +1,58 @@
+package it.gov.pagopa.pu.sil.controller;
+
+import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
+import it.gov.pagopa.pu.sendnotification.dto.generated.CreateNotificationRequest;
+import it.gov.pagopa.pu.sendnotification.dto.generated.CreateNotificationResponse;
+import it.gov.pagopa.pu.sil.service.notification.NotificationRetrieverService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationControllerTest {
+  @Mock
+  private NotificationRetrieverService notificationRetrieverServiceMock;
+  @InjectMocks
+  private NotificationController notificationController;
+  private UserInfo userInfo;
+
+  @BeforeEach
+  void setUp() {
+    userInfo = new UserInfo();
+    userInfo.setMappedExternalUserId("fakeExternalUser");
+    Authentication authentication = new UsernamePasswordAuthenticationToken(userInfo, "fakeAccessToken");
+    SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+    securityContext.setAuthentication(authentication);
+    SecurityContextHolder.setContext(securityContext);
+  }
+
+  @Test
+  void givenCreateSendNotificationThenOk() {
+    Long organizationId = 1L;
+    CreateNotificationRequest request = new CreateNotificationRequest();
+    CreateNotificationResponse expectedResult = new CreateNotificationResponse();
+
+    Mockito.when(notificationRetrieverServiceMock.createSendNotification(
+      organizationId,
+      request,
+      userInfo,
+      "fakeAccessToken"
+    )).thenReturn(expectedResult);
+
+    ResponseEntity<CreateNotificationResponse> response = notificationController.createSendNotification(organizationId,request);
+
+    Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    Assertions.assertSame(expectedResult, response.getBody());
+  }
+}
--- src/test/java/it/gov/pagopa/pu/sil/security/WebSecurityConfigTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/security/WebSecurityConfigTest.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.pu.sil.security;
 
+import it.gov.pagopa.pu.sil.controller.generated.NotificationApi;
 import it.gov.pagopa.pu.sil.service.AuthorizationService;
+import it.gov.pagopa.pu.sil.service.notification.NotificationRetrieverService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,17 +12,17 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+@WebMvcTest(value = {NotificationApi.class}, includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
   classes = JwtAuthenticationFilter.class) )
 @Import(WebSecurityConfig.class)
 class WebSecurityConfigTest {
 
   @Autowired
   private MockMvc mockMvc;
-
+  @MockitoBean
+  private NotificationRetrieverService notificationRetrieverService;
   @MockitoBean
   private AuthorizationService authorizationServiceMock;
 
