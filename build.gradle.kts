19695d663089c9ad16f3ac1d4076707a66cf2d4e
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-03-17 11:17:58 +0100

    feat: Http connection pooling (#12)

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ repositories {
 val springDocOpenApiVersion = "2.8.5"
 val openApiToolsVersion = "0.2.6"
 val micrometerVersion = "1.4.3"
+val httpClientVersion = "5.4.2"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
@@ -45,6 +46,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
+  implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
