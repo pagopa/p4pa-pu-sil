53fe1fb34ae5367f51db58a7eb606d2ef1bbeac3
antonio.torre <antonio.torre@pagopa.it>
2025-05-12 19:40:51 +0200

    fix: Removed EntityModel prefix from SpringDataRest generated schema on openApi

--- src/test/java/it/gov/pagopa/pu/sil/OpenApiGeneratorTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/OpenApiGeneratorTest.java
@@ -41,8 +41,7 @@ class OpenApiGeneratorTest {
       .andReturn();
 
     String openApiResult = result.getResponse().getContentAsString()
-      .replace("\r", "")
-      .replace("EntityModel", "");
+      .replace("\r", "");
 
     Assertions.assertTrue(openApiResult.startsWith("{\n  \"openapi\" : \"3."));
 
