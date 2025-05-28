88ca6296bf1c695050571414ece399c7261e1a63
macacia <marco.cacia@eng.it>
2025-05-28 12:04:54 +0200

    feat: P4ADEV-2891 IngestionFlowFile upload url generator (#34)

---
+++ src/main/java/it/gov/pagopa/pu/sil/service/ingestionflowfile/IngestionFlowFileReservationServiceImpl.java
@@ -0,0 +1,65 @@
+package it.gov.pagopa.pu.sil.service.ingestionflowfile;
+
+import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFileRequestDTO.IngestionFlowFileTypeEnum;
+import it.gov.pagopa.pu.sil.connector.processexecutions.IngestionFlowFileService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Slf4j
+@Service
+public class IngestionFlowFileReservationServiceImpl implements IngestionFlowFileReservationService {
+  private static final String FILE_ORIGIN = "SIL";
+  private static final String UNKNOWN = "UNKNOWN";
+
+  private final IngestionFlowFileService ingestionFlowFileService;
+  private final String fileShareBaseUrl;
+
+  public IngestionFlowFileReservationServiceImpl(IngestionFlowFileService ingestionFlowFileService,
+                                                 @Value("${public-base-url.fileshare}") String fileSharePublicBaseUrl) {
+    this.ingestionFlowFileService = ingestionFlowFileService;
+    this.fileShareBaseUrl = fileSharePublicBaseUrl;
+  }
+
+  @Override
+  public String uploadUrlGenerator(IngestionFlowFileTypeEnum ingestionFlowFileType,
+                                   Long organizationId,
+                                   String accessToken) {
+
+    log.info("Creating ingestion flow file reservation for type: {}, organizationId: {}",
+      ingestionFlowFileType, organizationId);
+
+    IngestionFlowFileRequestDTO requestDTO = mapToReservationRequest(ingestionFlowFileType, organizationId);
+
+    Long ingestionFlowFileId = ingestionFlowFileService.createIngestionFlowFileReservation(requestDTO, accessToken);
+    log.debug("Reservation created with ID: {}", ingestionFlowFileId);
+    requestDTO.setIngestionFlowFileId(ingestionFlowFileId);
+
+    String uploadUrl = composeUploadUrl(requestDTO);
+    log.info("Generated upload URL: {}", uploadUrl);
+
+    return uploadUrl;
+  }
+
+  private IngestionFlowFileRequestDTO mapToReservationRequest(IngestionFlowFileTypeEnum ingestionFlowFileType, Long organizationId) {
+    return new IngestionFlowFileRequestDTO()
+      .organizationId(organizationId)
+      .filePathName(UNKNOWN)
+      .fileName(UNKNOWN)
+      .fileSize(0L)
+      .ingestionFlowFileType(ingestionFlowFileType)
+      .fileOrigin(FILE_ORIGIN);
+  }
+
+  private String composeUploadUrl(IngestionFlowFileRequestDTO requestDTO) {
+    return UriComponentsBuilder.fromUriString(fileShareBaseUrl)
+      .path("/organization/{organizationId}/ingestionflowfiles")
+      .queryParam("ingestionFlowFileId", requestDTO.getIngestionFlowFileId())
+      .queryParam("ingestionFlowFileType", requestDTO.getIngestionFlowFileType())
+      .queryParam("fileOrigin", requestDTO.getFileOrigin())
+      .buildAndExpand(requestDTO.getOrganizationId())
+      .toUriString();
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/sil/service/ingestionflowfile/IngestionFlowFileReservationServiceTest.java
@@ -0,0 +1,65 @@
+package it.gov.pagopa.pu.sil.service.ingestionflowfile;
+
+import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFileRequestDTO;
+import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFileRequestDTO.IngestionFlowFileTypeEnum;
+import it.gov.pagopa.pu.sil.connector.processexecutions.IngestionFlowFileService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class IngestionFlowFileReservationServiceTest {
+  @Mock
+  IngestionFlowFileService ingestionFlowFileServiceMock;
+  private IngestionFlowFileReservationService service;
+  private final String baseUrl = "baseUrl";
+
+  @BeforeEach
+  void setUp() {
+    service = new IngestionFlowFileReservationServiceImpl(ingestionFlowFileServiceMock, baseUrl);
+  }
+
+  @Test
+  void whenUploadUrlGeneratorThenOk() {
+    // Given
+    String accessToken = "accessToken";
+    Long ingestionFlowFileId = 1L;
+    Long organizationId = 123L;
+    IngestionFlowFileTypeEnum fileType = IngestionFlowFileTypeEnum.ORGANIZATIONS_SIL_SERVICE;
+
+    // Capture the request DTO being sent to the service
+    ArgumentCaptor<IngestionFlowFileRequestDTO> requestCaptor = ArgumentCaptor.forClass(IngestionFlowFileRequestDTO.class);
+
+    when(ingestionFlowFileServiceMock.createIngestionFlowFileReservation(requestCaptor.capture(), eq(accessToken)))
+      .thenReturn(ingestionFlowFileId);
+
+    // When
+    String resultUrl = service.uploadUrlGenerator(fileType, organizationId, accessToken);
+
+    // Then
+    // Verify the correct request was made
+    IngestionFlowFileRequestDTO capturedRequest = requestCaptor.getValue();
+    assertEquals(organizationId, capturedRequest.getOrganizationId());
+    assertEquals("UNKNOWN", capturedRequest.getFilePathName());
+    assertEquals("UNKNOWN", capturedRequest.getFileName());
+    assertEquals(0L, capturedRequest.getFileSize());
+    assertEquals(fileType, capturedRequest.getIngestionFlowFileType());
+    assertEquals("SIL", capturedRequest.getFileOrigin());
+
+    // Verify the URL is constructed correctly
+    String expectedUrl = baseUrl +
+                        "/organization/" + organizationId +
+                        "/ingestionflowfiles" +
+                        "?ingestionFlowFileId=" + ingestionFlowFileId +
+                        "&ingestionFlowFileType=" + fileType +
+                        "&fileOrigin=SIL";
+
+    assertEquals(expectedUrl, resultUrl);
+  }
+}
