4fe2529b1ff123208bb7650013a21f81926c28d3
serdimic <sergio.dimichele@eng.it>
2025-06-13 13:06:52 +0200

    add private constructor to ValidationUtils

--- src/main/java/it/gov/pagopa/pu/sil/util/ValidationUtils.java
+++ src/main/java/it/gov/pagopa/pu/sil/util/ValidationUtils.java
@@ -24,6 +24,9 @@ public class ValidationUtils {
     "^(?:[A-Z][AEIOUX][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$");
   private static final Pattern FISCAL_CODE_LEGAL_ENTITY_PATTERN = Pattern.compile("^\\d{11}$");
 
+  private ValidationUtils() {
+    // Utility class, no instantiation
+  }
 
   public static boolean isValidDatiSpecificiRiscossione(final String datiSpecificiRiscossione) {
     return datiSpecificiRiscossione!=null && DATI_SPECIFICI_RISCOSSIONE_PATTERN.matcher(datiSpecificiRiscossione).matches();
