c0103eb6f908565424b92732a38586f3313a7de0
serdimic <sergio.dimichele@eng.it>
2025-06-18 12:30:59 +0200

    fix typo

--- src/main/java/it/gov/pagopa/pu/sil/service/immediatepayments/ValidationService.java
+++ src/main/java/it/gov/pagopa/pu/sil/service/immediatepayments/ValidationService.java
@@ -60,7 +60,7 @@ public class ValidationService {
   }
 
   public Pair<SilFaults, String> validatePaymentData(CtDatiSingoloVersamentoDovuti versamento) {
-    if (versamento.getImportoSingoloVersamento() == null || BigDecimal.ZERO.compfareTo(versamento.getImportoSingoloVersamento()) >= 0) {
+    if (versamento.getImportoSingoloVersamento() == null || BigDecimal.ZERO.compareTo(versamento.getImportoSingoloVersamento()) >= 0) {
       return Pair.of(SilFaults.PAA_IMPORTO_SINGOLO_VERSAMENTO_NON_VALIDO, "Importo singolo versamento non valido: " + versamento.getImportoSingoloVersamento());
     }
     if (StringUtils.isBlank(versamento.getDatiSpecificiRiscossione()) || !ValidationUtils.isValidLegacyPaymentMetadata(versamento.getDatiSpecificiRiscossione())) {
