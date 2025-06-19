3ca68775c2b22a089d5fd742473b3a9223f69db3
macacia <marco.cacia@eng.it>
2025-06-19 10:34:03 +0200

    client

---
+++ src/main/java/it/gov/pagopa/pu/sil/connector/amountupdates/client/AmountUpdatesLegacyClient.java
@@ -0,0 +1,22 @@
+package it.gov.pagopa.pu.sil.connector.amountupdates.client;
+
+import it.gov.pagopa.amountupdates.legacy.dto.generated.Credentials;
+import it.gov.pagopa.amountupdates.legacy.dto.generated.Token;
+import it.gov.pagopa.pu.sil.connector.amountupdates.config.AmountUpdatesApisHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AmountUpdatesLegacyClient {
+  private final AmountUpdatesApisHolder amountUpdatesApisHolder;
+
+  public AmountUpdatesLegacyClient(AmountUpdatesApisHolder amountUpdatesApisHolder) {
+    this.amountUpdatesApisHolder = amountUpdatesApisHolder;
+  }
+
+  public Token login(Credentials credentials, String authUrl) {
+    return amountUpdatesApisHolder.getAmountUpdatesLegacyApiClientByBaseUrl(authUrl)
+        .login(credentials);
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/sil/connector/amountupdates/client/AmountUpdatesLegacyClientTest.java
@@ -0,0 +1,54 @@
+package it.gov.pagopa.pu.sil.connector.amountupdates.client;
+
+import it.gov.pagopa.amountupdates.legacy.controller.generated.DefaultApi;
+import it.gov.pagopa.amountupdates.legacy.dto.generated.Credentials;
+import it.gov.pagopa.amountupdates.legacy.dto.generated.Token;
+import it.gov.pagopa.pu.sil.connector.amountupdates.config.AmountUpdatesApisHolder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AmountUpdatesLegacyClientTest {
+  @Mock
+  private AmountUpdatesApisHolder amountUpdatesApisHolderMock;
+  @Mock
+  private DefaultApi amountUpdatesLegacyApiClientMock;
+
+  private AmountUpdatesLegacyClient client;
+
+  @BeforeEach
+  void setUp() {
+    client = new AmountUpdatesLegacyClient(amountUpdatesApisHolderMock);
+  }
+
+  @AfterEach
+  void tearDown() {
+    Mockito.verifyNoMoreInteractions(amountUpdatesApisHolderMock);
+  }
+
+  @Test
+  void whenLoginThenInvokeClient() {
+    // Given
+    String authUrl = "http://example.com/auth";
+    Credentials credential = new Credentials("username", "password");
+    Token expectedToken = new Token();
+
+    Mockito.when(amountUpdatesApisHolderMock.getAmountUpdatesLegacyApiClientByBaseUrl(authUrl))
+           .thenReturn(amountUpdatesLegacyApiClientMock);
+    Mockito.when(amountUpdatesLegacyApiClientMock.login(credential))
+           .thenReturn(expectedToken);
+
+    // When
+    Token result = client.login(credential, authUrl);
+
+    // Then
+    assertSame(expectedToken, result);
+  }
+}
