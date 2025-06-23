2da31919c7a79fb687ca29fd9f5a9943b71a52e4
serdimic <sergio.dimichele@eng.it>
2025-06-23 11:45:17 +0200

    add javadoc for DebtPositionService.createDebtPosition

--- src/main/java/it/gov/pagopa/pu/sil/connector/debtpositions/DebtPositionService.java
+++ src/main/java/it/gov/pagopa/pu/sil/connector/debtpositions/DebtPositionService.java
@@ -9,5 +9,12 @@ public interface DebtPositionService {
   DebtPositionTypeOrg getDebtPositionTypeOrgByOrgIdAndType(Long organizationId, String debtPositionTypeOrgCode, String accessToken);
   DebtPositionType getDebtPositionTypeById(Long debtPositionType, String accessToken);
   Long countExistingInstallmentsByIudIuvNav(Long organizationId, String iud, String iuv, String nav, String accessToken);
+  /**
+   * Creates a new debt position.
+   *
+   * @param debtPositionDTO the DTO containing the details of the debt position to create
+   * @param accessToken the access token for authentication
+   * @return a pair containing the created DebtPositionDTO and the workflow ID from the response headers
+   */
   Pair<DebtPositionDTO, String> createDebtPosition(DebtPositionDTO debtPositionDTO, String accessToken);
 }
