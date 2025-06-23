3fed192aff74616c9958d5cdcd9416cbea81dce1
macacia <marco.cacia@eng.it>
2025-06-23 11:28:26 +0200

    arrange connector

--- src/main/java/it/gov/pagopa/pu/sil/connector/actualization/LegacyActualizationServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/sil/connector/actualization/LegacyActualizationServiceImpl.java
@@ -5,14 +5,19 @@ import it.gov.pagopa.actualization.legacy.dto.generated.Pagamento;
 import it.gov.pagopa.actualization.legacy.dto.generated.PagamentoAggiornato;
 import it.gov.pagopa.actualization.legacy.dto.generated.Token;
 import it.gov.pagopa.pu.sil.connector.actualization.client.LegacyActualizationClient;
+import it.gov.pagopa.pu.sil.dto.generated.AmountUpdatesDTO;
+import it.gov.pagopa.pu.sil.mapper.AmountUpdatesMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LegacyActualizationServiceImpl implements LegacyActualizationService {
   private final LegacyActualizationClient legacyActualizationClient;
+  private final AmountUpdatesMapper amountUpdatesMapper;
 
-  public LegacyActualizationServiceImpl(LegacyActualizationClient legacyActualizationClient) {
+  public LegacyActualizationServiceImpl(LegacyActualizationClient legacyActualizationClient,
+                                        AmountUpdatesMapper amountUpdatesMapper) {
     this.legacyActualizationClient = legacyActualizationClient;
+    this.amountUpdatesMapper = amountUpdatesMapper;
   }
 
   @Override
@@ -21,7 +26,8 @@ public class LegacyActualizationServiceImpl implements LegacyActualizationServic
   }
 
   @Override
-  public PagamentoAggiornato actualization(String accessToken, String serviceUrl, Pagamento pagamento) {
-    return legacyActualizationClient.actualization(accessToken, serviceUrl, pagamento);
+  public AmountUpdatesDTO actualization(String accessToken, String serviceUrl, Pagamento pagamento) {
+    PagamentoAggiornato actualization = legacyActualizationClient.actualization(accessToken, serviceUrl, pagamento);
+    return amountUpdatesMapper.pagamentoAggiornato2AmountUpdatesDTO(actualization);
   }
 }
