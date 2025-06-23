6a5c3f2f67b3c147dffe1133b09d79ec27f7163a
macacia <marco.cacia@eng.it>
2025-06-23 17:36:46 +0200

    removed unused

--- src/main/java/it/gov/pagopa/pu/sil/service/legacyauth/AuthConfigChain.java
+++ src/main/java/it/gov/pagopa/pu/sil/service/legacyauth/AuthConfigChain.java
@@ -1,8 +1,8 @@
 package it.gov.pagopa.pu.sil.service.legacyauth;
 
-import it.gov.pagopa.pu.organization.dto.generated.SilServiceAuthConfig;
+import it.gov.pagopa.pu.organization.dto.generated.OrgSilServiceRequestBodyAuthConfig;
 
-public class AuthConfigChain<T extends SilServiceAuthConfig, R> {
+public class AuthConfigChain<T extends OrgSilServiceRequestBodyAuthConfig, R> {
     private final T config;
     private final AbstractAuthConfigService<T, R> service;
 
