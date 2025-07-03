5220fec9483c08a74f6d7bfea369ff76c3ce6ce3
macacia <marco.cacia@eng.it>
2025-07-03 17:07:45 +0200

    add test

--- src/test/java/it/gov/pagopa/pu/sil/endpoint/PuForOrganizationPaymentsEndpointTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/endpoint/PuForOrganizationPaymentsEndpointTest.java
@@ -162,6 +162,24 @@ class PuForOrganizationPaymentsEndpointTest {
     Assertions.assertEquals(ExportFileLegacyStatus.fromValue2LegacyValue(processingStatus.getLeft()), response.getStato());
     Assertions.assertEquals(processingStatus.getRight(), response.getDownloadUrl());
   }
+
+  @Test
+  void givenGenericExceptionWhenPivotSILChiediStatoExportFlussoThenSystemErrorFault() throws Exception {
+    PaaSILChiediStatoExportFlusso request = podamFactory.manufacturePojo(PaaSILChiediStatoExportFlusso.class);
+    IntestazionePPT intestazionePPT = podamFactory.manufacturePojo(IntestazionePPT.class);
+    intestazionePPT.setCodIpaEnte(VALID_ORG_IPA_CODE);
+    SoapHeaderElement header = TestUtils.createSoapHeaderElement(intestazionePPT, IntestazionePPT.class);
+
+    Mockito.when(exportFileProcessingStatusServiceMock
+      .getProcessingStatus(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())
+    ).thenThrow(new RuntimeException("Unexpected error"));
+
+    PaaSILChiediStatoExportFlussoRisposta response =
+      puForOrganizationPaymentsEndpoint.paaSILChiediStatoExportFlusso(request, header);
+
+    Assertions.assertNotNull(response.getFault());
+    Assertions.assertEquals(SilFaults.PAA_SYSTEM_ERROR.code(), response.getFault().getFaultCode());
+  }
   // endregion
 
   // region PaaSILChiediStatoImportFlusso
