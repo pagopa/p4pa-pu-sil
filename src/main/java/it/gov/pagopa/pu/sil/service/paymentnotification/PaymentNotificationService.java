b5fdcd7286627fb2019882f200ebbbeee4f5e94f
macacia <marco.cacia@eng.it>
2025-07-04 12:56:40 +0200

    arrange mappers

--- src/main/java/it/gov/pagopa/pu/sil/service/paymentnotification/PaymentNotificationService.java
+++ src/main/java/it/gov/pagopa/pu/sil/service/paymentnotification/PaymentNotificationService.java
@@ -2,45 +2,99 @@ package it.gov.pagopa.pu.sil.service.paymentnotification;
 
 import it.gov.pagopa.paymentnotification.legacy.dto.generated.PaymentNotification;
 import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDTO;
 import it.gov.pagopa.pu.organization.dto.generated.OrgSilService;
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
+import it.gov.pagopa.pu.sil.connector.debtpositions.DebtPositionService;
 import it.gov.pagopa.pu.sil.connector.organization.service.OrgSilServiceComponent;
+import it.gov.pagopa.pu.sil.connector.organization.service.OrganizationService;
 import it.gov.pagopa.pu.sil.connector.paymentnotification.LegacyPaymentNotificationService;
+import it.gov.pagopa.pu.sil.exception.PaymentNotFoundException;
+import it.gov.pagopa.pu.sil.mapper.PagatiMapper;
 import it.gov.pagopa.pu.sil.service.AccessTokenService;
 import it.gov.pagopa.pu.sil.service.AuthorizationService;
+import it.gov.pagopa.pu.sil.service.receipt.ReceiptService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class PaymentNotificationService {
   private final OrgSilServiceComponent orgSilServiceComponent;
   private final LegacyPaymentNotificationService legacyPaymentNotificationService;
   private final AccessTokenService accessTokenService;
-
-  public PaymentNotificationService(OrgSilServiceComponent orgSilServiceComponent,
-                                    LegacyPaymentNotificationService legacyPaymentNotificationService,
-                                    AccessTokenService accessTokenService) {
-    this.orgSilServiceComponent = orgSilServiceComponent;
-    this.legacyPaymentNotificationService = legacyPaymentNotificationService;
-    this.accessTokenService = accessTokenService;
-  }
+  private final OrganizationService organizationService;
+  private final DebtPositionService deptPositionService;
+  private final PagatiMapper pagatiMapper;
+  private final ReceiptService receiptService;
 
   public void notifyPayment(Long orgSilServiceId, String nav, UserInfo loggedUser, String accessToken) {
     OrgSilService orgSilService = orgSilServiceComponent.getOrgSilServiceById(orgSilServiceId, accessToken)
       .orElseThrow(() -> new IllegalArgumentException("Organization service not found"));
     AuthorizationService.validateUserForOrganizationId(orgSilService.getOrganizationId(), loggedUser);
 
-    //TODO add integration logic here of https://pagopa.atlassian.net/browse/P4ADEV-3282
+    Organization organization = organizationService.getOrganizationById(orgSilService.getOrganizationId(), accessToken)
+      .orElse(null);
+
+    List<InstallmentDTO> installmentDTOs = deptPositionService.getInstallmentsByOrganizationIdAndNav(
+      orgSilService.getOrganizationId(),
+      nav,
+      null,
+      accessToken
+    );
+
+    List<Pair<DebtPositionDTO, InstallmentDTO>> debtPositionWithInstallmentList = getDebtPositionsAndInstallments(accessToken, installmentDTOs);
+
+    PaymentNotification paymentNotification = buildPaymentNotification(debtPositionWithInstallmentList, organization, accessToken);
 
     String silAccessToken = accessTokenService.getAccessToken(orgSilService, accessToken);
 
     legacyPaymentNotificationService.notifyPayment(
       silAccessToken,
       orgSilService.getServiceUrl(),
-      PaymentNotification.builder()
-        .rt("")
-        .esito("")
-        .build()
+      paymentNotification
     );
+  }
+
+  private List<Pair<DebtPositionDTO, InstallmentDTO>> getDebtPositionsAndInstallments(String accessToken, List<InstallmentDTO> installmentDTOs) {
+    List<Pair<DebtPositionDTO, InstallmentDTO>> debtPositionWithInstallmentList = installmentDTOs.stream()
+      .map(InstallmentDTO::getInstallmentId)
+      //search for the debt position by installmentId
+      .map(installmentId -> Pair.of(installmentId, deptPositionService.getDebtPositionByInstallmentId(installmentId, accessToken)))
+      //find the installment in the debt position
+      .map(debtPositionPair -> Pair.of(debtPositionPair.getRight(), findInstallmentOfDebtPosition(debtPositionPair.getRight(),
+        installment -> Objects.equals(installment.getInstallmentId(), debtPositionPair.getLeft()))))
+      //return the pair of debt position and matching installment
+      .toList();
+    return debtPositionWithInstallmentList;
+  }
+
+  private InstallmentDTO findInstallmentOfDebtPosition(DebtPositionDTO debtPosition, Predicate<InstallmentDTO> installmentFinderPredicate) {
+    return debtPosition.getPaymentOptions().stream()
+      .flatMap(po -> po.getInstallments().stream())
+      .filter(installmentFinderPredicate)
+      .findFirst()
+      .orElseThrow(() -> new PaymentNotFoundException("Installment not found"));
+  }
+
+  private PaymentNotification buildPaymentNotification(List<Pair<DebtPositionDTO, InstallmentDTO>> debtPositionWithInstallmentList,
+                                                       Organization organization, String accessToken) {
+    DebtPositionDTO debtPositionDTO = debtPositionWithInstallmentList.getFirst().getLeft();
+    InstallmentDTO installmentDTO = debtPositionWithInstallmentList.getFirst().getRight();
+    byte[] encodedPagati = pagatiMapper.mapDebtPositionsToEncodedPagati(debtPositionDTO, installmentDTO, organization, accessToken);
+    byte[] encodedReceipt = receiptService.getReceiptById(installmentDTO.getReceiptId(), organization.getOrganizationId(), accessToken);
+    return PaymentNotification.builder()
+      .rt(Base64.getEncoder().encodeToString(encodedReceipt))
+      .esito(Base64.getEncoder().encodeToString(encodedPagati))
+      .build();
   }
 }
--- src/test/java/it/gov/pagopa/pu/sil/service/paymentnotification/PaymentNotificationServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/service/paymentnotification/PaymentNotificationServiceTest.java
@@ -3,24 +3,37 @@ package it.gov.pagopa.pu.sil.service.paymentnotification;
 import it.gov.pagopa.paymentnotification.legacy.dto.generated.PaymentNotification;
 import it.gov.pagopa.pu.auth.dto.generated.AccessToken;
 import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionStatus;
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDTO;
+import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
 import it.gov.pagopa.pu.organization.dto.generated.OrgSilService;
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import it.gov.pagopa.pu.sil.connector.organization.service.OrgSilServiceComponent;
 import it.gov.pagopa.pu.sil.connector.paymentnotification.LegacyPaymentNotificationService;
+import it.gov.pagopa.pu.sil.exception.PaymentNotFoundException;
 import it.gov.pagopa.pu.sil.service.AccessTokenService;
 import it.gov.pagopa.pu.sil.service.AuthorizationService;
+import it.gov.pagopa.pu.sil.connector.debtpositions.DebtPositionService;
+import it.gov.pagopa.pu.sil.connector.organization.service.OrganizationService;
+import it.gov.pagopa.pu.sil.mapper.PagatiMapper;
+import it.gov.pagopa.pu.sil.service.receipt.ReceiptService;
+import it.gov.pagopa.pu.sil.util.TestUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import uk.co.jemos.podam.api.PodamFactory;
 
+import java.util.Base64;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentNotificationServiceTest {
@@ -30,21 +43,41 @@ class PaymentNotificationServiceTest {
   LegacyPaymentNotificationService legacyPaymentNotificationServiceMock;
   @Mock
   private AccessTokenService accessTokenServiceMock;
+  @Mock
+  private OrganizationService organizationServiceMock;
+  @Mock
+  private DebtPositionService debtPositionServiceMock;
+  @Mock
+  private PagatiMapper pagatiMapperMock;
+  @Mock
+  private ReceiptService receiptServiceMock;
 
+  @InjectMocks
   private PaymentNotificationService service;
+
+  private final PodamFactory podamFactory = TestUtils.getPodamFactory();
 
   @BeforeEach
   void setUp() {
-    service = new PaymentNotificationService(orgSilServiceComponentMock,
-      legacyPaymentNotificationServiceMock, accessTokenServiceMock);
+    service = new PaymentNotificationService(
+            orgSilServiceComponentMock,
+            legacyPaymentNotificationServiceMock,
+            accessTokenServiceMock,
+            organizationServiceMock,
+            debtPositionServiceMock,
+            pagatiMapperMock,
+            receiptServiceMock
+    );
   }
 
   @Test
   void whenNotifyPaymentThenOk() {
+    byte[] encodedPagati = "pagati".getBytes();
+    byte[] encodedReceipt = "receipt".getBytes();
     Long organizationId = 2L;
     Long orgSilServiceId = 1L;
     String nav = "NAV123";
-    UserInfo loggedUser = Mockito.mock(UserInfo.class);
+    UserInfo loggedUser = mock(UserInfo.class);
     String token = "token";
     AccessToken accessToken = new AccessToken()
       .accessToken("token")
@@ -55,35 +88,103 @@ class PaymentNotificationServiceTest {
       .flagLegacy(true)
       .serviceUrl("http://service.url");
     PaymentNotification paymentNotification = new PaymentNotification()
-      .rt("RTS123")
-      .esito("OK");
+      .rt(Base64.getEncoder().encodeToString(encodedReceipt))
+      .esito(Base64.getEncoder().encodeToString(encodedPagati));
+    Organization organization = mock(Organization.class);
+    DebtPositionDTO debtPositionDTO = podamFactory.manufacturePojo(DebtPositionDTO.class);
+    debtPositionDTO.setOrganizationId(organization.getOrganizationId());
+    debtPositionDTO.setStatus(DebtPositionStatus.PAID);
+    InstallmentDTO installmentDTO = debtPositionDTO.getPaymentOptions().getFirst().getInstallments().getFirst();
+    installmentDTO.setInstallmentId(1l);
+    installmentDTO.setStatus(InstallmentStatus.PAID);
+    List<InstallmentDTO> installmentDTOs = List.of(installmentDTO);
 
-    Mockito.when(accessTokenServiceMock.getAccessToken(orgSilService, token))
-      .thenReturn(accessToken.getAccessToken());
-    Mockito.when(orgSilServiceComponentMock.getOrgSilServiceById(orgSilService.getOrgSilServiceId(), accessToken.getAccessToken()))
+    when(orgSilServiceComponentMock.getOrgSilServiceById(orgSilService.getOrgSilServiceId(), accessToken.getAccessToken()))
       .thenReturn(Optional.of(orgSilService));
-    Mockito.doNothing().when(legacyPaymentNotificationServiceMock)
+    when(organizationServiceMock.getOrganizationById(orgSilService.getOrganizationId(), accessToken.getAccessToken()))
+        .thenReturn(Optional.of(organization));
+    when(debtPositionServiceMock.getInstallmentsByOrganizationIdAndNav(
+        orgSilService.getOrganizationId(), nav, null, accessToken.getAccessToken()))
+      .thenReturn(installmentDTOs);
+    when(debtPositionServiceMock.getDebtPositionByInstallmentId(installmentDTO.getInstallmentId(), accessToken.getAccessToken()))
+      .thenReturn(debtPositionDTO);
+    when(pagatiMapperMock.mapDebtPositionsToEncodedPagati(debtPositionDTO, installmentDTO, organization, accessToken.getAccessToken()))
+      .thenReturn(encodedPagati);
+    when(receiptServiceMock.getReceiptById(installmentDTO.getReceiptId(), organization.getOrganizationId(), accessToken.getAccessToken()))
+      .thenReturn(encodedReceipt);
+    when(accessTokenServiceMock.getAccessToken(orgSilService, token))
+      .thenReturn(accessToken.getAccessToken());
+    doNothing().when(legacyPaymentNotificationServiceMock)
       .notifyPayment(accessToken.getAccessToken(), orgSilService.getServiceUrl(), paymentNotification);
-
-    assertDoesNotThrow(() ->
-      service.notifyPayment(orgSilServiceId, nav, loggedUser, token));
+    try (MockedStatic<AuthorizationService> authService = mockStatic(AuthorizationService.class)) {
+      authService.when(() -> AuthorizationService.validateUserForOrganizationId(organizationId, loggedUser)).then(Answers.RETURNS_DEEP_STUBS);
+      assertDoesNotThrow(() -> service.notifyPayment(orgSilServiceId, nav, loggedUser, token));
+    }
   }
 
   @Test
-  void whenOrgSilServiceNotFoundThenThrowsException() {
+  void whenInstallmentNotFoundThenPaymentNotFoundException() {
+    Long organizationId = 2L;
     Long orgSilServiceId = 1L;
     String nav = "NAV123";
-    UserInfo loggedUser = Mockito.mock(UserInfo.class);
+    UserInfo loggedUser = mock(UserInfo.class);
+    String token = "token";
+    AccessToken accessToken = new AccessToken()
+      .accessToken("token")
+      .tokenType("Bearer");
+    OrgSilService orgSilService = new OrgSilService()
+      .organizationId(organizationId)
+      .orgSilServiceId(orgSilServiceId)
+      .flagLegacy(true)
+      .serviceUrl("http://service.url");
+
+    Organization organization = mock(Organization.class);
+    InstallmentDTO installmentDTO = mock(InstallmentDTO.class);
+    List<InstallmentDTO> installmentDTOs = List.of(installmentDTO);
+    DebtPositionDTO debtPositionDTO = mock(DebtPositionDTO.class);
+
+    when(orgSilServiceComponentMock.getOrgSilServiceById(orgSilService.getOrgSilServiceId(), accessToken.getAccessToken()))
+      .thenReturn(Optional.of(orgSilService));
+    when(organizationServiceMock.getOrganizationById(orgSilService.getOrganizationId(), accessToken.getAccessToken()))
+      .thenReturn(Optional.of(organization));
+    when(debtPositionServiceMock.getInstallmentsByOrganizationIdAndNav(
+      orgSilService.getOrganizationId(), nav, null, accessToken.getAccessToken()))
+      .thenReturn(installmentDTOs);
+    when(debtPositionServiceMock.getDebtPositionByInstallmentId(installmentDTO.getInstallmentId(), accessToken.getAccessToken()))
+      .thenReturn(debtPositionDTO);
+
+    try (MockedStatic<AuthorizationService> authService = mockStatic(AuthorizationService.class)) {
+      authService.when(() -> AuthorizationService.validateUserForOrganizationId(organizationId, loggedUser)).then(Answers.RETURNS_DEEP_STUBS);
+      assertThrows(PaymentNotFoundException.class, () -> service.notifyPayment(orgSilServiceId, nav, loggedUser, token));
+    }
+  }
+
+  @Test
+  void whenOrgSilServiceNotFoundThenThrowsIllegalArgumentException() {
+    Long orgSilServiceId = 1L;
+    String nav = "NAV123";
+    UserInfo loggedUser = mock(UserInfo.class);
     String accessToken = "token";
+
+    when(orgSilServiceComponentMock.getOrgSilServiceById(orgSilServiceId, accessToken)).thenReturn(Optional.empty());
+
+    Assertions.assertThrows(IllegalArgumentException.class, () ->
+      service.notifyPayment(orgSilServiceId, nav, loggedUser, accessToken)
+    );
+  }
+
+  @Test
+  void whenValidateUserForOrganizationIdFailsThenAuthorizationDeniedException() {
+    Long orgSilServiceId = 1L;
     Long organizationId = 2L;
-
-    Mockito.when(orgSilServiceComponentMock.getOrgSilServiceById(orgSilServiceId, accessToken)).thenReturn(Optional.empty());
-
-    try (MockedStatic<AuthorizationService> authService = Mockito.mockStatic(AuthorizationService.class)) {
-      authService.when(() -> AuthorizationService.validateUserForOrganizationId(organizationId, loggedUser)).thenAnswer(Answers.RETURNS_DEFAULTS);
-      Assertions.assertThrows(IllegalArgumentException.class, () ->
-        service.notifyPayment(orgSilServiceId, nav, loggedUser, accessToken)
-      );
+    String nav = "NAV123";
+    String token = "token";
+    UserInfo loggedUser = mock(UserInfo.class);
+    OrgSilService orgSilService = new OrgSilService().organizationId(organizationId).orgSilServiceId(orgSilServiceId).flagLegacy(true).serviceUrl("http://service.url");
+    when(orgSilServiceComponentMock.getOrgSilServiceById(orgSilServiceId, token)).thenReturn(Optional.of(orgSilService));
+    try (MockedStatic<AuthorizationService> authService = mockStatic(AuthorizationService.class)) {
+      authService.when(() -> AuthorizationService.validateUserForOrganizationId(organizationId, loggedUser)).thenThrow(AuthorizationDeniedException.class);
+      assertThrows(AuthorizationDeniedException.class, () -> service.notifyPayment(orgSilServiceId, nav, loggedUser, token));
     }
   }
 }
