ac97b1a35ffa37d991cd1d0ccfa0f6ace611f679
macacia <marco.cacia@eng.it>
2025-07-28 09:52:59 +0200

    assertion addition

--- src/test/java/it/gov/pagopa/pu/sil/mapper/InstantPaymentMapperTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/mapper/InstantPaymentMapperTest.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.pu.sil.mapper;
 
-import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionDTO;
-import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionTypeOrg;
+import it.gov.pagopa.pu.debtpositions.dto.generated.*;
 import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import it.gov.pagopa.pu.organization.dto.generated.OrganizationStatus;
 import it.gov.pagopa.pu.sil.connector.debtpositions.DebtPositionTypeService;
@@ -59,23 +58,45 @@ class InstantPaymentMapperTest {
     assertNotNull(result);
     assertEquals(1, result.size());
 
-    result.forEach(dp -> {
-      TestUtils.checkNotNullFields(dp,
-        "debtPositionId", "validityDate", "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
-      dp.getPaymentOptions().forEach(po -> {
-        TestUtils.checkNotNullFields(po, "paymentOptionId", "debtPositionId", "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
-        po.getInstallments().forEach(i -> {
-          TestUtils.checkNotNullFields(i, "installmentId", "paymentOptionId", "syncStatus", "iupdPagopa",
-            "iuv", "iur", "iuf", "nav", "iun", "notificationFeeCents", "transfers", "notificationDate", "ingestionFlowFileId",
-            "ingestionFlowFileAction", "ingestionFlowFileLineNumber", "receiptId", "switchToExpired",
-            "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
-          if(i.getTransfers()!=null) {
-            i.getTransfers().forEach(t -> {
-              TestUtils.checkNotNullFields(t, "transferId", "installmentId",
-                "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
-            });
-          }
-        });
+    DebtPositionDTO dp = result.getFirst();
+
+    assertEquals(org.getOrganizationId(), dp.getOrganizationId());
+    assertEquals(paymentDTO.getDescription(), dp.getDescription());
+    assertTrue(dp.getIupdOrg().contains("CART_ID"));
+    assertEquals(DebtPositionStatus.UNPAID, dp.getStatus());
+    assertEquals(DebtPositionOrigin.SPONTANEOUS_SIL, dp.getDebtPositionOrigin());
+
+    PaymentOptionDTO po = dp.getPaymentOptions().getFirst();
+    assertEquals(PaymentOptionStatus.UNPAID, po.getStatus());
+    assertEquals(1, po.getPaymentOptionIndex());
+    assertEquals(PaymentOptionTypeEnum.SINGLE_INSTALLMENT, po.getPaymentOptionType());
+    assertEquals(paymentDTO.getTotalAmountCents(), po.getTotalAmountCents());
+    assertEquals(transferDTO.getRemittanceInformation(), po.getDescription());
+
+    InstallmentDTO i = po.getInstallments().getFirst();
+    assertEquals(InstallmentStatus.UNPAID, i.getStatus());
+    assertEquals(paymentDTO.getIud(), i.getIud());
+    assertEquals(transferDTO.getAmountCents(), i.getAmountCents());
+    assertEquals(paymentDTO.getDebtor(), i.getDebtor());
+    assertEquals(transferDTO.getCategory(), i.getLegacyPaymentMetadata());
+    assertEquals(transferDTO.getRemittanceInformation(), i.getRemittanceInformation());
+    assertTrue(i.getSourceFlowName().contains("CART_ID"));
+
+    TestUtils.checkNotNullFields(dp,
+      "debtPositionId", "validityDate", "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
+    dp.getPaymentOptions().forEach(po2 -> {
+      TestUtils.checkNotNullFields(po2, "paymentOptionId", "debtPositionId", "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
+      po2.getInstallments().forEach(i2 -> {
+        TestUtils.checkNotNullFields(i2, "installmentId", "paymentOptionId", "syncStatus", "iupdPagopa",
+          "iuv", "iur", "iuf", "nav", "iun", "notificationFeeCents", "notificationDate", "ingestionFlowFileId",
+          "ingestionFlowFileAction", "ingestionFlowFileLineNumber", "receiptId", "switchToExpired",
+          "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
+        if(i2.getTransfers()!=null) {
+          i2.getTransfers().forEach(t -> {
+            TestUtils.checkNotNullFields(t, "transferId", "installmentId",
+              "creationDate", "updateDate", "updateOperatorExternalId", "updateTraceId");
+          });
+        }
       });
     });
   }
