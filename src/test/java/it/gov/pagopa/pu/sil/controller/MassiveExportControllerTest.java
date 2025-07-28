dfa6c141abf4d257ad126c2b1886f3d295991f9b
barozzi <luca.barozzi@eng.it>
2025-07-28 12:11:34 +0200

    P4ADEV-3435 Implement native API status-export test fix and generated-sil.openapi.json

--- src/test/java/it/gov/pagopa/pu/sil/controller/MassiveExportControllerTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/controller/MassiveExportControllerTest.java
@@ -4,9 +4,12 @@ import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
 import it.gov.pagopa.pu.sil.dto.generated.ClassificationsExportRequestDTO;
 import it.gov.pagopa.pu.sil.dto.generated.ExportFileResponseDTO;
 import it.gov.pagopa.pu.sil.dto.generated.PaidExportRequestDTO;
+import it.gov.pagopa.pu.sil.security.SecurityUtilsTest;
 import it.gov.pagopa.pu.sil.service.AuthorizationService;
+import it.gov.pagopa.pu.sil.service.AuthorizationServiceTest;
 import it.gov.pagopa.pu.sil.service.exportfile.ClassificationsExportFileReservationService;
 import it.gov.pagopa.pu.sil.service.exportfile.PaidExportFileReservationService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,10 +20,6 @@ import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
@@ -28,73 +27,66 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class MassiveExportControllerTest {
   @Mock
-  private PaidExportFileReservationService paidExportFileReservationService;
+  private PaidExportFileReservationService paidExportFileReservationServiceMock;
   @Mock
-  private ClassificationsExportFileReservationService classificationsExportFileReservationService;
+  private ClassificationsExportFileReservationService classificationsExportFileReservationServiceMock;
 
   @InjectMocks
   private MassiveExportController controller;
 
+  private final String accessToken = "fakeAccessToken";
+  private final String orgFiscalCode = "ORG123456789";
+  private final String orgIpaCode = "userIpaCode";
   private UserInfo userInfo;
 
   @BeforeEach
   void setUp() {
-    userInfo = new UserInfo();
+    userInfo = AuthorizationServiceTest.buildAdminUser(1L, orgFiscalCode, orgIpaCode);
     userInfo.setMappedExternalUserId("fakeExternalUser");
-    Authentication authentication = new UsernamePasswordAuthenticationToken(userInfo, "fakeAccessToken");
-    SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-    securityContext.setAuthentication(authentication);
-    SecurityContextHolder.setContext(securityContext);
+    SecurityUtilsTest.configureSecurityContext(accessToken, userInfo);
+
+  }
+
+  @AfterEach
+  void clear(){
+    SecurityUtilsTest.clearSecurityContext();
   }
 
   @Test
   void whenMassiveExportRequestPaidThenOk() {
     // Given
-    String orgFiscalCode = "ORG123456789";
-    String accessToken = "fakeAccessToken";
-    String userIpaCode = "userIpaCode";
-
     String expectedExportId = "1";
 
-    try (MockedStatic<AuthorizationService> authMock = mockStatic(AuthorizationService.class)) {
-      authMock.when(() -> AuthorizationService.getOrgIpaCodeFromUserInfo(userInfo, orgFiscalCode))
-        .thenReturn(userIpaCode);
+    PaidExportRequestDTO paidExportRequestDTO = new PaidExportRequestDTO();
 
-      PaidExportRequestDTO paidExportRequestDTO = new PaidExportRequestDTO();
+    when(paidExportFileReservationServiceMock
+      .doReservation(userInfo, accessToken, orgIpaCode, paidExportRequestDTO))
+      .thenReturn(Long.valueOf(expectedExportId));
 
-      when(paidExportFileReservationService
-        .doReservation(userInfo, accessToken, userIpaCode, paidExportRequestDTO))
-        .thenReturn(Long.valueOf(expectedExportId));
+    // When
+    ResponseEntity<ExportFileResponseDTO> response = controller
+      .massivePaidExportRequest(orgFiscalCode, paidExportRequestDTO);
 
-      // When
-      ResponseEntity<ExportFileResponseDTO> response = controller
-        .massivePaidExportRequest(orgFiscalCode, paidExportRequestDTO);
-
-      // Then
-      Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-      Assertions.assertNotNull(response.getBody());
-      Assertions.assertEquals(expectedExportId, response.getBody().getExportId());
-    }
+    // Then
+    Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    Assertions.assertNotNull(response.getBody());
+    Assertions.assertEquals(expectedExportId, response.getBody().getExportId());
 
   }
 
   @Test
   void whenMassiveExportRequestClassificationsThenOk() {
     // Given
-    String orgFiscalCode = "ORG123456789";
-    String accessToken = "fakeAccessToken";
-    String userIpaCode = "userIpaCode";
-
     String expectedExportId = "1";
 
     try (MockedStatic<AuthorizationService> authMock = mockStatic(AuthorizationService.class)) {
       authMock.when(() -> AuthorizationService.getOrgIpaCodeFromUserInfo(userInfo, orgFiscalCode))
-        .thenReturn(userIpaCode);
+        .thenReturn(orgIpaCode);
 
       ClassificationsExportRequestDTO classificationsExportRequestDTO = new ClassificationsExportRequestDTO();
 
-      when(classificationsExportFileReservationService
-        .doReservation(userInfo, accessToken, userIpaCode, classificationsExportRequestDTO))
+      when(classificationsExportFileReservationServiceMock
+        .doReservation(userInfo, accessToken, orgIpaCode, classificationsExportRequestDTO))
         .thenReturn(Long.valueOf(expectedExportId));
 
       // When
