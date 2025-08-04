28d9344b777518e6c4730e9f9087af4a58e90cd3
macacia <marco.cacia@eng.it>
2025-08-04 17:48:58 +0200

    fix

--- src/main/java/it/gov/pagopa/pu/sil/service/ingestionflowfile/IngestionFlowFileProcessingStatusService.java
+++ src/main/java/it/gov/pagopa/pu/sil/service/ingestionflowfile/IngestionFlowFileProcessingStatusService.java
@@ -16,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -47,9 +48,10 @@ public class IngestionFlowFileProcessingStatusService {
       .status(ingestionFlowFile.getStatus())
       .build();
 
-    if (IngestionFlowFileStatus.COMPLETED.equals(ingestionFlowFile.getStatus())) {
+    if (ingestionFlowFile.getStatus().equals(IngestionFlowFileStatus.COMPLETED) || ingestionFlowFile.getStatus().equals(IngestionFlowFileStatus.ERROR)) {
       addDownloadUrlsToResponse(responseDTO, ingestionFlowFile);
     }
+
     return responseDTO;
   }
 
@@ -64,11 +66,14 @@ public class IngestionFlowFileProcessingStatusService {
   }
 
   private void addDownloadUrlsToResponse(ImportStatusResponseDTO responseDTO, IngestionFlowFile ingestionFlowFile) {
-    responseDTO.addDownloadUrlsItem(new DownloadUrl(CodeEnum.OUTPUT_FILE, composeUrl(ingestionFlowFile, "/imported")));
-    if (ingestionFlowFile.getDiscardFileName() != null) {
+    boolean success = ingestionFlowFile.getErrorDescription() == null;
+    if (ingestionFlowFile.getNumCorrectlyImportedRows() > 0) {
+      responseDTO.addDownloadUrlsItem(new DownloadUrl(CodeEnum.OUTPUT_FILE, composeUrl(ingestionFlowFile, "/imported")));
+    }
+    if (!success && ingestionFlowFile.getDiscardFileName() != null) {
       responseDTO.addDownloadUrlsItem(new DownloadUrl(CodeEnum.DISCARDED_FILE, composeUrl(ingestionFlowFile, "/errors")));
     }
-    if (ingestionFlowFile.getPdfGeneratedId() != null) {
+    if (success && ingestionFlowFile.getPdfGeneratedId() != null) {
       responseDTO.addDownloadUrlsItem(new DownloadUrl(CodeEnum.PAYMENT_NOTICE_FILE, composeUrl(ingestionFlowFile, "/notice")));
     }
   }
