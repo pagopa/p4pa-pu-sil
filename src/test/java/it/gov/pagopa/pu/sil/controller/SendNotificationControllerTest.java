534ab3f4cc7af5b6d897f3e16ad38f6b892785f7
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2025-10-03 13:15:41 +0200

    P4ADEV-3805-uniform-pu-sil-api removed unused imports from SendNotificationControllerTest (fixed Sonar Issues)

--- src/test/java/it/gov/pagopa/pu/sil/controller/SendNotificationControllerTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/controller/SendNotificationControllerTest.java
@@ -16,10 +16,6 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Collections;
 import java.util.List;
