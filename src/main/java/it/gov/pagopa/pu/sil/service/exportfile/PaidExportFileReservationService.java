1210d5edd123d2c1c3eebc6d6370eef8c0efd1c9
pelliccm <matteo.pelliccione@nttdata.com>
2025-10-16 15:31:36 +0200

    [P4ADEV-3904] Handles IdentificativoTipoDovuto SECONDARIO

--- src/main/java/it/gov/pagopa/pu/sil/service/exportfile/PaidExportFileReservationService.java
+++ src/main/java/it/gov/pagopa/pu/sil/service/exportfile/PaidExportFileReservationService.java
@@ -3,25 +3,29 @@ package it.gov.pagopa.pu.sil.service.exportfile;
 import it.gov.pagopa.pu.auth.dto.generated.UserInfo;
 import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionTypeOrg;
 import it.gov.pagopa.pu.processexecutions.dto.generated.PaidExportFileFilter;
+import it.gov.pagopa.pu.processexecutions.dto.generated.PaidExportFileFilter.DebtPositionOriginsEnum;
 import it.gov.pagopa.pu.processexecutions.dto.generated.PaidExportFileRequestDTO;
 import it.gov.pagopa.pu.sil.connector.debtpositions.DebtPositionTypeService;
 import it.gov.pagopa.pu.sil.connector.processexecutions.ExportFileService;
 import it.gov.pagopa.pu.sil.dto.generated.PaidExportRequestDTO;
 import it.gov.pagopa.pu.sil.enums.SilFaults;
 import it.gov.pagopa.pu.sil.service.AuthorizationService;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Slf4j
 @Service
-public class PaidExportFileReservationService extends AbstractExportFileReservationService {
+public class PaidExportFileReservationService extends
+  AbstractExportFileReservationService {
+  public static final String IDENTIFICATIVO_TIPO_DOVUTO_SECONDARIO = "SECONDARIO";
 
   private final ExportFileService exportFileService;
 
   public PaidExportFileReservationService(ExportFileService exportFileService,
-                                          DebtPositionTypeService debtPositionTypeService) {
+    DebtPositionTypeService debtPositionTypeService) {
     super(debtPositionTypeService);
     this.exportFileService = exportFileService;
   }
@@ -34,32 +38,55 @@ public class PaidExportFileReservationService extends AbstractExportFileReservat
 
     AuthorizationService.validateAdminRole(orgIpaCode, userInfo);
     Long organizationId = getOrganizationIdFromUserInfo(userInfo, orgIpaCode);
-    Long debtPositionTypeOrgId =
-      Optional.ofNullable(paidExportRequestDTO.getExportFilters().getDebtPositionTypeOrgCode())
-        .map(debtPositionTypeOrgCode -> getAndValidateDebtPositionTypeOrg(
-          organizationId,
-          debtPositionTypeOrgCode,
-          accessToken,
-          SilFaults.PAA_IDENTIFICATIVO_TIPO_DOVUTO_NON_VALIDO,
-          SilFaults.PAA_IDENTIFICATIVO_TIPO_DOVUTO_NON_ABILITATO
-        )).map(DebtPositionTypeOrg::getDebtPositionTypeOrgId).orElse(null);
 
-    PaidExportFileRequestDTO requestDTO = mapToExportRequest(organizationId, debtPositionTypeOrgId, paidExportRequestDTO);
-    Long exportFileId = exportFileService.createPaidExportFile(requestDTO, accessToken);
+    Long debtPositionTypeOrgId = null;
+    if (!IDENTIFICATIVO_TIPO_DOVUTO_SECONDARIO.equals(
+      paidExportRequestDTO.getExportFilters().getDebtPositionTypeOrgCode())) {
+      debtPositionTypeOrgId =
+        Optional.ofNullable(
+            paidExportRequestDTO.getExportFilters().getDebtPositionTypeOrgCode())
+          .map(debtPositionTypeOrgCode -> getAndValidateDebtPositionTypeOrg(
+            organizationId,
+            debtPositionTypeOrgCode,
+            accessToken,
+            SilFaults.PAA_IDENTIFICATIVO_TIPO_DOVUTO_NON_VALIDO,
+            SilFaults.PAA_IDENTIFICATIVO_TIPO_DOVUTO_NON_ABILITATO
+          )).map(DebtPositionTypeOrg::getDebtPositionTypeOrgId).orElse(null);
+    }
+
+    PaidExportFileRequestDTO requestDTO = mapToExportRequest(organizationId,
+      debtPositionTypeOrgId, paidExportRequestDTO);
+    Long exportFileId = exportFileService.createPaidExportFile(requestDTO,
+      accessToken);
     log.debug("Export file created with ID: {}", exportFileId);
     return exportFileId;
   }
 
   private PaidExportFileRequestDTO mapToExportRequest(Long organizationId,
-                                                      Long debtPositionTypeOrgId,
-                                                      PaidExportRequestDTO paidExportRequestDTO) {
+    Long debtPositionTypeOrgId,
+    PaidExportRequestDTO paidExportRequestDTO) {
+    PaidExportFileFilter filters = new PaidExportFileFilter()
+      .installmentUpdateDateTime(
+        paidExportRequestDTO.getExportFilters().getInstallmentUpdateDateTime())
+      .paymentDateTime(
+        paidExportRequestDTO.getExportFilters().getPaymentDateTime());
+
+    if (IDENTIFICATIVO_TIPO_DOVUTO_SECONDARIO.equals(
+      paidExportRequestDTO.getExportFilters().getDebtPositionTypeOrgCode())) {
+      filters.setDebtPositionOrigins(
+        List.of(DebtPositionOriginsEnum.SECONDARY_ORG));
+    } else {
+      filters.setDebtPositionTypeOrgId(debtPositionTypeOrgId);
+      filters.setDebtPositionOrigins(
+        Arrays.stream(DebtPositionOriginsEnum.values()).filter(
+            o -> !DebtPositionOriginsEnum.SECONDARY_ORG.equals(o))
+          .toList());
+    }
+
     return new PaidExportFileRequestDTO()
       .organizationId(organizationId)
       .exportFileType(PaidExportFileRequestDTO.ExportFileTypeEnum.PAID)
       .fileVersion(paidExportRequestDTO.getFileVersion())
-      .filterFields(new PaidExportFileFilter()
-        .debtPositionTypeOrgId(debtPositionTypeOrgId)
-        .installmentUpdateDateTime(paidExportRequestDTO.getExportFilters().getInstallmentUpdateDateTime())
-        .paymentDateTime(paidExportRequestDTO.getExportFilters().getPaymentDateTime()));
+      .filterFields(filters);
   }
 }
