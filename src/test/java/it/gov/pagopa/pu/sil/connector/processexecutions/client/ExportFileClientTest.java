0b374b3858f72bc96e02b6dbefd0a792e75d5e45
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-11-10 11:20:17 +0100

    fix: P4ADEV-4148 fix prenotaExport (#181)

--- src/test/java/it/gov/pagopa/pu/sil/connector/processexecutions/client/ExportFileClientTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/connector/processexecutions/client/ExportFileClientTest.java
@@ -91,7 +91,7 @@ class ExportFileClientTest {
       .thenReturn(exportFileControllerApiMock);
 
     ProcessExecutionsErrorDTO processExecutionsErrorDTO = new ProcessExecutionsErrorDTO(
-      ProcessExecutionsErrorDTO.CodeEnum.PROCESS_EXECUTIONS_INVALID_TIME_RANGE, "Invalid time range");
+      ProcessExecutionsErrorDTO.CodeEnum.PROCESS_EXECUTIONS_INVALID_TIME_RANGE, "Invalid time range", "traceId");
     HttpClientErrorException.BadRequest badRequest = (HttpClientErrorException.BadRequest) HttpClientErrorException.create(
       HttpStatus.BAD_REQUEST, "Bad request", null, null, null);
     badRequest.setBodyConvertFunction(t -> processExecutionsErrorDTO);
