e207dbe4610e932cc32731a3b64d9a0f6ba06217
ElisKina-dev <elis.kina@nttdata.com>
2025-11-17 16:05:20 +0100

    fix ManageDebtPositionWithIudDTO

--- src/main/java/it/gov/pagopa/pu/sil/dto/ManageDebtPositionWithIudDTO.java
+++ src/main/java/it/gov/pagopa/pu/sil/dto/ManageDebtPositionWithIudDTO.java
@@ -3,21 +3,25 @@ package it.gov.pagopa.pu.sil.dto;
 import it.gov.pagopa.pu.sil.dto.generated.ManageDebtPositionDTO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder(toBuilder = true)
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class ManageDebtPositionWithIudDTO extends ManageDebtPositionDTO {
   private String iud;
 
   public static ManageDebtPositionWithIudDTO fromManageDebtPositionDTO(ManageDebtPositionDTO dto, String iud) {
-    return ManageDebtPositionWithIudDTO.builder()
-      .debtPositionDescription(dto.getDebtPositionDescription())
-      .validityDate(dto.getValidityDate())
-      .paymentOptionDescription(dto.getPaymentOptionDescription())
-      .installments(dto.getInstallments())
-      .iud(iud)
-      .build();
+    ManageDebtPositionWithIudDTO enrichedDTO = new ManageDebtPositionWithIudDTO();
+
+    enrichedDTO.setDebtPositionDescription(dto.getDebtPositionDescription());
+    enrichedDTO.setValidityDate(dto.getValidityDate());
+    enrichedDTO.setPaymentOptionDescription(dto.getPaymentOptionDescription());
+    enrichedDTO.setInstallments(dto.getInstallments());
+    enrichedDTO.setIud(iud);
+
+    return enrichedDTO;
   }
 }
