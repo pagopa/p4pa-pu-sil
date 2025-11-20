4af38a8bd57dbf71ec7f3f9c22d01650f88783b6
macacia <marco.cacia@eng.it>
2025-11-20 16:33:41 +0100

    merge

--- src/main/java/it/gov/pagopa/pu/sil/controller/DebtorQueryPaymentController.java
+++ src/main/java/it/gov/pagopa/pu/sil/controller/DebtorQueryPaymentController.java
@@ -5,9 +5,11 @@ import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
 import it.gov.pagopa.pu.debtpositions.dto.generated.PersonEntityType;
 import it.gov.pagopa.pu.sil.controller.generated.DebtorQueryPaymentApi;
 import it.gov.pagopa.pu.sil.dto.generated.PaymentHistoryResponseDTO;
+import it.gov.pagopa.pu.sil.dto.generated.UnpaidDebtPositionsResponseDTO;
 import it.gov.pagopa.pu.sil.security.SecurityUtils;
 import it.gov.pagopa.pu.sil.service.querypayments.AbstractDebtorQueryPaymentService.DebtorQueryPaymentRequest;
 import it.gov.pagopa.pu.sil.service.querypayments.DebtorQueryPaymentService;
+import it.gov.pagopa.pu.sil.service.querypayments.DebtorQueryUnpaidDebtPositionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +20,12 @@ import java.time.OffsetDateTime;
 @RestController
 public class DebtorQueryPaymentController implements DebtorQueryPaymentApi {
   private final DebtorQueryPaymentService debtorQueryPaymentService;
+  private final DebtorQueryUnpaidDebtPositionService debtorQueryUnpaidDebtPositionService;
 
-  public DebtorQueryPaymentController(DebtorQueryPaymentService debtorQueryPaymentService) {
+  public DebtorQueryPaymentController(DebtorQueryPaymentService debtorQueryPaymentService,
+                                      DebtorQueryUnpaidDebtPositionService debtorQueryUnpaidDebtPositionService) {
     this.debtorQueryPaymentService = debtorQueryPaymentService;
+    this.debtorQueryUnpaidDebtPositionService = debtorQueryUnpaidDebtPositionService;
   }
 
   @Override
@@ -37,5 +42,21 @@ public class DebtorQueryPaymentController implements DebtorQueryPaymentApi {
     );
 
     return ResponseEntity.ok(debtorQueryPaymentService.processRequest(request, userInfo, accessToken));
+  }
+
+  @Override
+  public ResponseEntity<UnpaidDebtPositionsResponseDTO> getUnpaidDebtPositions(String debtorFiscalCode, PersonEntityType debtorEntityType, String ipaCode) {
+    UserInfo userInfo = SecurityUtils.getLoggedUser();
+    String accessToken = SecurityUtils.getAccessToken();
+    DebtorQueryPaymentRequest request = new DebtorQueryPaymentRequest(
+      ipaCode,
+      debtorEntityType,
+      debtorFiscalCode,
+      InstallmentStatus.UNPAID,
+      null,
+      null
+    );
+
+    return ResponseEntity.ok(debtorQueryUnpaidDebtPositionService.processRequest(request, userInfo, accessToken));
   }
 }
