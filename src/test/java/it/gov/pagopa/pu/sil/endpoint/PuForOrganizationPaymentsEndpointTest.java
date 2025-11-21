7c521390e1370af2dfb5c835c94776020a94c9da
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-11-21 11:31:04 +0100

    fix: P4ADEV-4199 handle null dates in prenotaExport (#191)

--- src/test/java/it/gov/pagopa/pu/sil/endpoint/PuForOrganizationPaymentsEndpointTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/endpoint/PuForOrganizationPaymentsEndpointTest.java
@@ -706,6 +706,42 @@ class PuForOrganizationPaymentsEndpointTest {
   }
 
   @Test
+  void givenRequestWithoutFromDateWhenPaaSILPrenotaExportFlussoThrowsClientExceptionThenResponseContainsExpectedFaultCode() throws Exception {
+    PaaSILPrenotaExportFlusso request = podamFactory.manufacturePojo(PaaSILPrenotaExportFlusso.class);
+    request.setDateFrom(null);
+    request.setIdentificativoTipoDovuto("THAT_TYPE");
+    IntestazionePPT intestazionePPT = podamFactory.manufacturePojo(IntestazionePPT.class);
+    intestazionePPT.setCodIpaEnte(VALID_ORG_IPA_CODE);
+    SoapHeaderElement header = TestUtils.createSoapHeaderElement(intestazionePPT, IntestazionePPT.class);
+
+    PaaSILPrenotaExportFlussoRisposta response = puForOrganizationPaymentsEndpoint.paaSILPrenotaExportFlusso(request, header);
+
+    Assertions.assertNotNull(response);
+    Assertions.assertEquals(SilFaults.PAA_DATE_FROM_NON_VALIDO.code(), response.getFault().getFaultCode());
+  }
+
+  @Test
+  void givenRequestWithoutDateToWhenPaaSILPrenotaExportFlussoThenOK() throws Exception {
+    PaaSILPrenotaExportFlusso request = podamFactory.manufacturePojo(PaaSILPrenotaExportFlusso.class);
+    request.setDateTo(null);
+    request.setIdentificativoTipoDovuto("THAT_TYPE");
+
+    IntestazionePPT intestazionePPT = podamFactory.manufacturePojo(IntestazionePPT.class);
+    intestazionePPT.setCodIpaEnte(VALID_ORG_IPA_CODE);
+    SoapHeaderElement header = TestUtils.createSoapHeaderElement(intestazionePPT, IntestazionePPT.class);
+    Long expectedToken = 12345L;
+
+    Mockito.when(paaSILPrenotaExportFlussoServiceMock.paaSILPrenotaExportFlusso(
+      Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()
+    )).thenReturn(expectedToken);
+
+    PaaSILPrenotaExportFlussoRisposta response = puForOrganizationPaymentsEndpoint.paaSILPrenotaExportFlusso(request, header);
+
+    Assertions.assertNotNull(response);
+    Assertions.assertEquals(String.valueOf(expectedToken), response.getRequestToken());
+  }
+
+  @Test
   void givenValidRequestWhenPaaSILPrenotaExportFlussoThrowsClientExceptionThenResponseContainsExpectedFaultCode() throws Exception {
     // Given
     PaaSILPrenotaExportFlusso request = podamFactory.manufacturePojo(PaaSILPrenotaExportFlusso.class);
