2ed1ed5cd178935778f7ba557e086c67870b5166
antonio.torre <antonio.torre@pagopa.it>
2025-12-23 22:08:37 +0100

    chore: Fix gradle import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import com.github.jk1.license.filter.*
 import com.github.jk1.license.render.*
 import java.util.*
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
