2b36f1289f6016da98e727227cb648bcf9f25a12
macacia <marco.cacia@eng.it>
2026-03-02 16:51:34 +0100

    non used imports

--- src/test/java/it/gov/pagopa/pu/sil/service/querypayments/PaaSILChiediPosizioniAperteServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/sil/service/querypayments/PaaSILChiediPosizioniAperteServiceTest.java
@@ -2,10 +2,7 @@ package it.gov.pagopa.pu.sil.service.querypayments;
 
 import static it.gov.pagopa.pu.sil.util.Constants.EXCLUDED_DEBT_POSITION_TYPE_CODES;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
