c72ddd6bb14bc27dc58ebe48e02d143b48ada545
antonio.torre <antonio.torre@pagopa.it>
2026-03-11 12:51:53 +0100

    chore: Refactor to use DebtPositionOrigin centralized aggregated Set

--- src/main/java/it/gov/pagopa/pu/sil/service/debtposition/InstallmentFacadeService.java
+++ src/main/java/it/gov/pagopa/pu/sil/service/debtposition/InstallmentFacadeService.java
@@ -1,21 +1,15 @@
 package it.gov.pagopa.pu.sil.service.debtposition;
 
-import it.gov.pagopa.pu.debtpositions.dto.generated.DebtPositionOrigin;
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentDTO;
 import it.gov.pagopa.pu.debtpositions.dto.generated.InstallmentStatus;
 import it.gov.pagopa.pu.sil.connector.debtpositions.InstallmentService;
+import it.gov.pagopa.pu.sil.util.Constants;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class InstallmentFacadeService {
-  public static final List<DebtPositionOrigin> ALLOWED_ORIGINS = List.of(
-    DebtPositionOrigin.ORDINARY,
-    DebtPositionOrigin.ORDINARY_SIL,
-    DebtPositionOrigin.SPONTANEOUS,
-    DebtPositionOrigin.SPONTANEOUS_SIL
-  );
 
   private final InstallmentService installmentService;
 
@@ -24,7 +18,7 @@ public class InstallmentFacadeService {
   }
 
   public List<InstallmentDTO> getInstallmentsByOrganizationIdAndNav(Long organizationId, String nav, String accessToken) {
-    return installmentService.getInstallmentsByOrganizationIdAndNav(organizationId, nav, ALLOWED_ORIGINS, accessToken)
+    return installmentService.getInstallmentsByOrganizationIdAndNav(organizationId, nav, Constants.ORDINARY_DEBT_POSITION_ORIGINS, accessToken)
       .stream()
       .filter(i -> i.getStatus() != InstallmentStatus.CANCELLED)
       .toList();
