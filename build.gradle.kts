190cac059103017ba94883ae734a594624836f12
antonio.torre <antonio.torre@pagopa.it>
2026-04-28 18:24:19 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.5"
+  id("org.springframework.boot") version "4.0.6"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "7.2.3.7755"
-  id("com.github.ben-manes.versions") version "0.53.0"
+  id("com.github.ben-manes.versions") version "0.54.0"
   id("org.openapi.generator") version "7.21.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "2.5.7"
@@ -52,11 +52,11 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.2"
+val springDocOpenApiVersion = "3.0.3"
 val janinoVersion = "3.1.12"
 val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.6.4"
-val httpClientVersion = "5.6"
+val micrometerVersion = "1.6.5"
+val httpClientVersion = "5.6.1"
 val httpCoreVersion = "5.4.2"
 val springWolfAsyncApiVersion = "1.20.0"
 val springWolfUiAsyncApiVersion = "1.20.0"
@@ -67,14 +67,11 @@ val activationVersion = "2.1.4"
 val xmlSchemaVersion = "2.3.2"
 val caffeineVersion = "3.2.3"
 val javaJwtVersion = "4.5.1"
-val jwksRsaVersion = "0.23.0"
-val bouncycastleVersion = "1.83"
+val jwksRsaVersion = "0.23.1"
+val bouncycastleVersion = "1.84"
 val nimbusJoseJwtVersion = "10.9"
 val commonsLang3Version = "3.20.0"
-val lz4JavaVersion = "1.10.4"
-
-// fix cve
-val jackson3CoreVersion = "3.1.1"
+val lz4JavaVersion = "1.11.0"
 
 val springCloudDepsVersion = "2025.1.1"
 
@@ -133,9 +130,6 @@ dependencies {
   jaxb("jakarta.activation:jakarta.activation-api:$activationVersion")
   jaxbext("com.github.jaxb-xew-plugin:jaxb-xew-plugin:2.1")
   jaxbext("org.jvnet.jaxb:jaxb-plugins:4.0.0")
-
-  // CVE fix
-  implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
