89438926db3a974d6f49385535aac0e4ae9d3fb7
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-09-02 12:50:51 +0200

    fix: Fix CVE (#283)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.1.0"
+  id("org.springframework.boot") version "4.1.1"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.3.1.8318"
+  id("org.sonarqube") version "7.4.0.8496"
   id("com.github.ben-manes.versions") version "0.54.0"
-  id("org.openapi.generator") version "7.23.0"
+  id("org.openapi.generator") version "7.25.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "4.0.1"
   //code generation for soap webservices classes (via  jaxb)
@@ -52,28 +52,30 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.3"
-val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.7.0"
+val springDocOpenApiVersion = "3.1.0"
+val openApiToolsVersion = "0.2.11"
+val micrometerVersion = "1.7.1"
 val httpClientVersion = "5.6.4"
 val httpCoreVersion = "5.4.3"
 val kafkaAppender = "0.2.0-RC2"
-val lz4JavaVersion = "1.11.1"
+val lz4JavaVersion = "1.11.2"
 val springWolfAsyncApiVersion = "1.21.0"
 val springWolfUiAsyncApiVersion = "1.21.0"
 val podamVersion = "8.0.2.RELEASE"
 val jaxbVersion = "4.0.9"
 val jaxbApiVersion = "4.0.5"
+val jaxbXewPluginVersion = "2.1"
+val jaxbPluginVersion = "4.0.16"
 val activationVersion = "2.1.4"
 val xmlSchemaVersion = "2.3.2"
 val caffeineVersion = "3.2.4"
-val javaJwtVersion = "4.5.2"
+val javaJwtVersion = "4.6.0"
 val jwksRsaVersion = "0.24.1"
-val bouncycastleVersion = "1.84"
+val bouncycastleVersion = "1.85.2"
 val nimbusJoseJwtVersion = "10.9.1"
 val commonsLang3Version = "3.20.0"
 
-val springCloudDepsVersion = "2025.1.2"
+val springCloudDepsVersion = "2025.1.3"
 
 dependencyManagement {
   imports {
@@ -103,11 +105,11 @@ dependencies {
     exclude(group = "org.apache.commons", module = "commons-lang3")
   }
   implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
-  implementation("io.github.springwolf:springwolf-kafka:${springWolfAsyncApiVersion}") {
+  implementation("io.github.springwolf:springwolf-kafka:$springWolfAsyncApiVersion") {
     exclude(group = "org.lz4", module = "lz4-java")
   }
-  implementation("io.github.springwolf:springwolf-ui:${springWolfUiAsyncApiVersion}")
-  implementation("io.github.springwolf:springwolf-cloud-stream:${springWolfAsyncApiVersion}")
+  implementation("io.github.springwolf:springwolf-ui:$springWolfUiAsyncApiVersion")
+  implementation("io.github.springwolf:springwolf-cloud-stream:$springWolfAsyncApiVersion")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
   implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
   implementation("org.apache.httpcomponents.core5:httpcore5-h2:$httpCoreVersion")
@@ -117,9 +119,9 @@ dependencies {
   }
 
   // validation token jwt
-  implementation("com.auth0:java-jwt:${javaJwtVersion}")
-  implementation("com.auth0:jwks-rsa:${jwksRsaVersion}")
-  implementation("org.bouncycastle:bcprov-jdk18on:${bouncycastleVersion}")
+  implementation("com.auth0:java-jwt:$javaJwtVersion")
+  implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
+  implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
 
   //webservice soap
   implementation("org.apache.ws.xmlschema:xmlschema-core:$xmlSchemaVersion")
@@ -131,8 +133,8 @@ dependencies {
   jaxb("com.sun.xml.bind:jaxb-core:$jaxbVersion")
   jaxb("jakarta.xml.bind:jakarta.xml.bind-api:$jaxbApiVersion")
   jaxb("jakarta.activation:jakarta.activation-api:$activationVersion")
-  jaxbext("com.github.jaxb-xew-plugin:jaxb-xew-plugin:2.1")
-  jaxbext("org.jvnet.jaxb:jaxb-plugins:4.0.0")
+  jaxbext("com.github.jaxb-xew-plugin:jaxb-xew-plugin:$jaxbXewPluginVersion")
+  jaxbext("org.jvnet.jaxb:jaxb-plugins:$jaxbPluginVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
@@ -143,7 +145,7 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-security-test")
   testImplementation("org.mockito:mockito-core")
   testImplementation("org.projectlombok:lombok")
-  testImplementation("uk.co.jemos.podam:podam:${podamVersion}")
+  testImplementation("uk.co.jemos.podam:podam:$podamVersion")
 }
 
 tasks.withType<Test> {
